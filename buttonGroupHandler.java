347b6be5f6d864436039889d0fceae2e9ee411bb
JeonSangHoon0711 <121441565+JeonSangHoon0711@users.noreply.github.com>
2023-02-15 15:15:20 +0900

    first

---
+++ buttonGroupHandler.java
@@ -0,0 +1,13 @@
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JRadioButton;
+
+public class buttonGroupHandler implements ItemListener{
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		
+	}
+
+}

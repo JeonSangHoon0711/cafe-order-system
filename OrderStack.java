2cdb3d536c5f6d027352000e6585d6bed3f1c243
JeonSangHoon0711 <121441565+JeonSangHoon0711@users.noreply.github.com>
2023-02-22 14:12:04 +0900

    Update OrderStack.java

--- OrderStack.java
+++ OrderStack.java
@@ -3,8 +3,3 @@ import java.util.Stack;
 public class OrderStack {
   static Stack<String> OrderStack = new Stack();
 }
-import java.util.Stack;
-
-public class OrderStack {
-  static Stack<String> OrderStack = new Stack();
-}
